89d71e31e2369db349301b4fa9e8e2dea7006afd
lsellens <lsellens@gmail.com>
2025-07-09 17:28:43 -0500

    update

--- jiggler/usb_descriptors.c
+++ jiggler/usb_descriptors.c
@@ -31,34 +31,34 @@
  * MSC (first), then CDC (later) will possibly cause system error on PC.
  *
  * Auto ProductID layout's Bitmap:
- *   [MSB]         HID | MSC | CDC          [LSB]
+ * [MSB]          HID | MSC | CDC            [LSB]
  */
 #define _PID_MAP(itf, n) ((CFG_TUD_##itf) << (n))
-#define USB_PID                                                                \
-  (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) |           \
+#define USB_PID                                                                    \
+  (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) |                \
    _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4))
 
 //--------------------------------------------------------------------+
 // Device Descriptors
 //--------------------------------------------------------------------+
 tusb_desc_device_t const desc_device = {.bLength = sizeof(tusb_desc_device_t),
-                                        .bDescriptorType = TUSB_DESC_DEVICE,
-                                        .bcdUSB = 0x0200,
-                                        .bDeviceClass = 0x00,
-                                        .bDeviceSubClass = 0x00,
-                                        .bDeviceProtocol = 0x00,
-                                        .bMaxPacketSize0 =
-                                            CFG_TUD_ENDPOINT0_SIZE,
+                                       .bDescriptorType = TUSB_DESC_DEVICE,
+                                       .bcdUSB = 0x0200,
+                                       .bDeviceClass = 0x00,
+                                       .bDeviceSubClass = 0x00,
+                                       .bDeviceProtocol = 0x00,
+                                       .bMaxPacketSize0 =
+                                           CFG_TUD_ENDPOINT0_SIZE,
 
-                                        .idVendor = 0xCafe,
-                                        .idProduct = USB_PID,
-                                        .bcdDevice = 0x0100,
+                                       .idVendor = 0xCafe,
+                                       .idProduct = USB_PID,
+                                       .bcdDevice = 0x0100,
 
-                                        .iManufacturer = 0x01,
-                                        .iProduct = 0x02,
-                                        .iSerialNumber = 0x03,
+                                       .iManufacturer = 0x01,
+                                       .iProduct = 0x02,
+                                       .iSerialNumber = 0x03,
 
-                                        .bNumConfigurations = 0x01};
+                                       .bNumConfigurations = 0x01};
 
 // Invoked when received GET DEVICE DESCRIPTOR
 // Application return pointer to descriptor
@@ -76,7 +76,10 @@ uint8_t const desc_hid_report[] = {
 // Invoked when received GET HID REPORT DESCRIPTOR
 // Application return pointer to descriptor
 // Descriptor contents must exist long enough for transfer to complete
-uint8_t const *tud_hid_descriptor_report_cb(void) { return desc_hid_report; }
+uint8_t const *tud_hid_descriptor_report_cb(uint8_t instance) {
+  (void)instance;
+  return desc_hid_report;
+}
 
 //--------------------------------------------------------------------+
 // Configuration Descriptor
@@ -96,7 +99,7 @@ uint8_t const desc_configuration[] = {
 
     // Interface number, string index, protocol, report descriptor len, EP In &
     // Out address, size & polling interval
-    TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, HID_PROTOCOL_NONE,
+    TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, 0, // Changed HID_PROTOCOL_NONE to 0
                        sizeof(desc_hid_report), EPNUM_HID, CFG_TUD_HID_BUFSIZE,
                        10)};
 
@@ -115,9 +118,9 @@ uint8_t const *tud_descriptor_configuration_cb(uint8_t index) {
 // array of pointer to string descriptors
 char const *string_desc_arr[] = {
     (const char[]){0x09, 0x04}, // 0: is supported language is English (0x0409)
-    "Raspberry Pi",             // 1: Manufacturer
-    "Mouse",                    // 2: Product
-    "123456",                   // 3: Serials, should use chip ID
+    "Raspberry Pi",              // 1: Manufacturer
+    "Mouse",                     // 2: Product
+    "123456",                    // 3: Serials, should use chip ID
 };
 
 static uint16_t _desc_str[32];
